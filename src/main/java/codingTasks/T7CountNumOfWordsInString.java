943332d56fb29e19597cd5dfe0bcdf76e303c910
Yaroslav Zaderaka <yzaderaka@gmail.com>
2025-06-18 14:34:37 +0200

    fix couple mistakes

--- src/main/java/codingTasks/T7CountNumOfWordsInString.java
+++ src/main/java/codingTasks/T7CountNumOfWordsInString.java
@@ -2,11 +2,14 @@ package codingTasks;
 
 public class T7CountNumOfWordsInString {
 
-    static int countWords(String str){
-       return str.split("[.,:;!?]").length;
+    static int countWords(String str) {
+
+        String[] words = str.trim().split("\\s+");
+        return words.length;
+
     }
 
     public static void main(String[] args) {
-        System.out.println(countWords("Yaroslav Zaderaka"));
+        System.out.println(countWords("Yaroslav   Zaderaka"));
     }
 }
--- src/main/java/codingTasks/T9FirstNonRepeatingCharacter.java
+++ src/main/java/codingTasks/T9FirstNonRepeatingCharacter.java
@@ -5,12 +5,10 @@ public class T9FirstNonRepeatingCharacter {
     public static Character findFirstNonRepeatingChar(String str) {
         int[] charCount = new int[256]; // Для ASCII символов
 
-        // Первый проход: подсчет частот
         for (char c : str.toCharArray()) {
             charCount[c]++;
         }
 
-        // Второй проход: поиск первого символа с частотой 1
         for (char c : str.toCharArray()) {
             if (charCount[c] == 1) {
                 return c;
